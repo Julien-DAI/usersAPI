848182aeff49b4393f89227b7cde2b9bb4aa27f2
Julien-DAI <julien.dai@imt-atlantique.net>
2023-12-12 19:51:08 +0100

    updateuser

--- src/services/usersAPI/controllers/user.controller.ts
+++ src/services/usersAPI/controllers/user.controller.ts
@@ -1,14 +1,14 @@
 import { FastifyReply, FastifyRequest } from "fastify"
-import { IUser } from "../interfaces"
+import { ExtendedUser, UserRole, IUser } from "../interfaces"
 import type * as s from 'zapatos/schema'
 import * as db from 'zapatos/db'
 import pool from '../../../common/db/pgPool'
 
 export const listUsers =
     async (request: FastifyRequest, reply: FastifyReply) => {
-/*        return db.sql<s.users.SQL, s.users.Selectable[]>`SELECT * FROM ${"users"}`
+        return db.sql<s.users.SQL, s.users.Selectable[]>`SELECT * FROM ${"users"}`
             .run(pool)
-            .then((users) => ({ data: users }))*/
+            .then((users) => ({ data: users }))
         // Or .then((users) => reply.send({ data: users }))
     }
 
@@ -16,13 +16,13 @@ export const listUsers =
 export const getUserById = async (
     request: FastifyRequest<{ Params: { id: number } }>,
     reply: FastifyReply
-) => {/*
+) => {
     const userId = request.params.id;
 
     try {
         const user = await db.sql<s.users.SQL, s.users.Selectable[]>`
       SELECT * FROM ${'users'}
-      WHERE ${'user_id'} = ${db.param(userId)}
+      WHERE id = ${db.param(userId)}
     `.run(pool);
 
         if (user.length === 0) {
@@ -33,48 +33,88 @@ export const getUserById = async (
     } catch (error) {
         console.error(error);
         reply.code(500).send({ error: 'Internal Server Error' });
-    }*/
+    }
 };
 
 export const addUser = async (
-    request: FastifyRequest<{ Body: Pick<s.users.Insertable, 'name' | 'password'> }>,
+    request: FastifyRequest<{ Body: Omit<ExtendedUser, 'id'> }>,
     reply: FastifyReply
 ) => {
-    /*const { name, password } = request.body;
+    const { name, role, password } = request.body;
 
     try {
-        const newUser = await db.insert('users', {
-            name,
-            password,
-        }).run(pool);
+        // Construct the newUser object based on the determined role
+        let newUser: Omit<ExtendedUser, 'id'>;
 
-        reply.send({ data: newUser });
+        switch (role) {
+            case 'player':
+                newUser = { name, role, badges:0, password } as Omit<ExtendedUser, 'id'>;
+                break;
+            case 'reporter':
+                newUser = { name, role, password };
+                break;
+            case 'admin':
+                newUser = { name, role, password };
+                break;
+            default:
+                // Handle unexpected role
+                throw new Error('Invalid user role');
+        }
+
+        // Insert the newUser into the database
+        const insertedUser = await db.insert('users', newUser).run(pool);
+
+        reply.send({ data: insertedUser });
     } catch (error) {
         console.error(error);
         reply.code(500).send({ error: 'Internal Server Error' });
-    }*/
+    }
 };
+
 
 export const updateUser = async (
     request: FastifyRequest<{ Params: { id: number }; Body: Partial<s.users.Updatable> }>,
     reply: FastifyReply
 ) => {
-    /*const userId = request.params.id;
+    const userId = request.params.id;
     const updatedData = request.body;
+    let updatedUsers = [];
 
     try {
-        const updatedUser = await db.update('users', updatedData, {
-            user_id: userId,
-        })
-            .run(pool);
+        if (updatedData.name!=null) {
+            updatedUsers = await db.sql<s.users.SQL, s.users.Selectable[]>`
+            UPDATE ${"users"}
+            SET ${"name"} = ${db.param(updatedData.name)}
+            WHERE ${"id"} = ${db.param(userId)}
+            RETURNING *;
+        `.run(pool);
+        }
+        if (updatedData.badges!=null) {
+            updatedUsers = await db.sql<s.users.SQL, s.users.Selectable[]>`
+            UPDATE ${"users"}
+            SET ${"badges"} = ${db.param(updatedData.badges)}
+            WHERE ${"id"} = ${db.param(userId)}
+            RETURNING *;
+        `.run(pool);
+        }
+        if (updatedData.password!=null) {
+            updatedUsers = await db.sql<s.users.SQL, s.users.Selectable[]>`
+            UPDATE ${"users"}
+            SET ${"password"} = ${db.param(updatedData.password)}
+            WHERE ${"id"} = ${db.param(userId)}
+            RETURNING *;
+        `.run(pool);
+        }
 
-        if (updatedUser.length === 0) {
+
+
+        if (updatedUsers.length === 0) {
             reply.code(404).send({ error: 'User not found' });
         } else {
-            reply.send({ data: updatedUser[0] });
+            reply.send({ data: updatedUsers[0] });
         }
     } catch (error) {
         console.error(error);
         reply.code(500).send({ error: 'Internal Server Error' });
-    }*/
+    }
 };
--- src/services/usersAPI/interfaces/index.ts
+++ src/services/usersAPI/interfaces/index.ts
@@ -1,5 +1,35 @@
-export interface IUser {
-    id: number
-    name: string
-    score?: number
+interface Player {
+    id: number;
+    name: string;
+    role: 'player';
+    badges: number;
+    password: string;
 }
+
+interface Reporter {
+    id: number;
+    name: string;
+    role: 'reporter';
+    password: string;
+
+}
+
+interface Admin {
+    id: number;
+    name: string;
+    role: 'admin';
+    password: string;
+
+}
+
+export type UserRole = 'player' | 'reporter' | 'admin';
+
+export interface IUser {
+    id: number;
+    name: string;
+    role: UserRole;
+    badges?: number; // Badges are optional for non-players
+
+}
+
+export type ExtendedUser = Player | Reporter | Admin;
